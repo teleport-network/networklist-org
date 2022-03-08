ad0373fa5f575d56a11cf746877a4d9603465250
Frank Wei <frank.wei@mirana.tech>
2022-03-08 17:01:21 +0800

    chore: typing for `web3` and `ethereum`

--- additional.d.ts
+++ additional.d.ts
@@ -3,3 +3,14 @@
  * you can include additional types by adding a new file e.g. additional.d.ts 
  * and then referencing it in the include array in your tsconfig.json.
  */
+declare global {
+    interface Window { 
+        ethereum?: any;
+        web3?: any;
+    }
+}
+
+/**
+ * This make Typescript recognize this file
+ */
+export {}
